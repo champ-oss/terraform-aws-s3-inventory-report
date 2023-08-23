b2db2e13ef6a452f0d00b16ae3cae1af8989407f
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-08-23 12:22:53 -0500

    resolve missing s3 bucket var

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -11,8 +11,15 @@ data "aws_iam_policy_document" "this" {
   }
 }
 
+module "s3" {
+  source  = "github.com/champ-oss/terraform-aws-s3.git?ref=v1.0.40-137c64b"
+  git     = "terraform-aws-s3-inventory"
+  protect = false
+}
+
 module "this" {
-  source        = "../../"
-  git           = var.git
-  lambda_policy = data.aws_iam_policy_document.this.json
+  source              = "../../"
+  git                 = var.git
+  lambda_policy       = data.aws_iam_policy_document.this.json
+  s3_inventory_bucket = module.s3.bucket
 }
